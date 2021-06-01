831442d0c0cbe621a6148b8a19ee1147832ed79b
Ersin Çatkaya <ersin_ctky@hotmail.com>
2021-06-01 23:06:46 +0300

    added slug field to questions

--- models/Question.js
+++ models/Question.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const slugify=require("slugify");
 const Schema = mongoose.Schema;
 
 const QuestionSchema = new Schema({
@@ -24,6 +25,21 @@ const QuestionSchema = new Schema({
         ref:"User"
     }
 });
+QuestionSchema.pre("save",function(next){
+    if(!this.isDirectModified("title")){
+        next();
+    }
+    this.slug = this.makeSlug();
+    next();
+});
+
+QuestionSchema.methods.makeSlug = function(){
+    return slugify(this.title,{
+        replacement:'-',
+        remove: /[*+~.()'"!:@]/g,
+        lower:true
+    })
+};
 
 
 module.exports= mongoose.model("Question",QuestionSchema);
